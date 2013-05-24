410fa1ca2f1aca0cec00f3dcb8440684025b449c
Antimorris <antimorris@jusanet.org>
2013-05-24 19:08:32 +0200

    Added initial code for Greedy Feed Forward Algorithm

---
+++ gffa.cpp
@@ -0,0 +1,22 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+class nodeQp {
+	int id,
+	string childrens, predicates;
+	public:
+	nodeQP (int);
+	void addChildren (string);
+	void addPredicate (string)
+};
+
+//////////////////////////////////////////////////////////////
+int main ()
+{
+	
+	exit(0);
+}
+//////////////////////////////////////////////////////////////
