98f0f7059cfaba4fee9ad567d89abd1644c24a07
Javier <antimorris@jusanet.org>
2013-05-24 19:22:15 +0200

    test cpp extension

---
+++ gffa.cpp
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+class nodeQp {
+	int id,
+	string childrens, definition;
+	public:
+	nodeQP (int);
+	void addChildren (string);
+	void addPredicate (string);
+	void getPredicates (void);
+	
+};
+
+//////////////////////////////////////////////////////////////
+int main ()
+{
+	
+	exit(0);
+}
+//////////////////////////////////////////////////////////////
