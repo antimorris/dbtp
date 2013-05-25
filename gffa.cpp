8d2e2f5d960649be2ba266847d6116517e890180
Javier <antimorris@jusanet.org>
2013-05-25 02:51:42 +0200

    Fixed data types within the functions

--- gffa.cpp
+++ gffa.cpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <vector>
 #include <map>
+#include <sstream>
 using namespace std;
 
 //////////////////////////////////////////////////////////////
@@ -15,7 +16,7 @@ class nodeQP {
 	int nodeId, nodeType;
 	string children, definition;
 	void addChildren (int);
-	void addPredicate (string);
+	void addPredicate (int);
 	vector<int> getChildren (void);
 	vector<string>	getPredicates (void);
 	nodeQP (int, int);
@@ -27,33 +28,49 @@ class nodeQP {
 //////////////////////////////////////////////////////////////
 class aip {
 	public:
-	int loc;
-	string key, predicate;
-	map<string,string> registry;
-	void addPredicate (int, string, string);
-	string checkPredicate (string);
-	string getPredicate (string);
+	int loc,predicate;
+	string key;
+	map<int,string> registry;
+        map<int,string>::iterator it;
+	void addPredicate (int, string, int);
+	string checkPredicate (int);
+	string getPredicate (int);
         void showFullRegistry (void);
 };
 
 //////////////////////////////////////////////////////////////
 // AIP:::addPredicate definition
 //////////////////////////////////////////////////////////////
-void aip::addPredicate (int a, string b, string c)
+void aip::addPredicate (int r_loc, string r_key, int r_pred)
 {
+        stringstream key;
+        key << r_loc << "," << r_key << "," << r_pred;
+        registry.insert(pair<int,string>(r_pred,key.str()));
 }
 
 //////////////////////////////////////////////////////////////
 // AIP::checkPredicate definition
 //////////////////////////////////////////////////////////////
-string aip::checkPredicate (string)
+string aip::checkPredicate (int key)
 {
+        map<string,string>::iterator it;
+
+        if (registry.find(key) == registry.end())
+        {
+                cout << "KEY DOES NOT EXIST\n";
+                return "NULL";
+        }
+        else
+        {
+                cout << "KEY EXISTSSSSSSS --> " << registry[key] << endl;
+                return registry[key];
+        }
 }
 
 //////////////////////////////////////////////////////////////
 // AIP::getPredicate definition
 //////////////////////////////////////////////////////////////
-string aip::getPredicate(string)
+string aip::getPredicate(int)
 {
 
 }
@@ -63,7 +80,10 @@ string aip::getPredicate(string)
 //////////////////////////////////////////////////////////////
 void aip::showFullRegistry(void)
 {
-
+        for (map<int,string>::iterator it=registry.begin(); it!=registry.end(); ++it)
+        {
+                cout << it->first << " => " << it->second << '\n';
+        }
 }
 
 //////////////////////////////////////////////////////////////
@@ -85,7 +105,7 @@ void nodeQP::addChildren (int id)
 //////////////////////////////////////////////////////////////
 // NodeQP addPredicate definition
 //////////////////////////////////////////////////////////////
-void nodeQP::addPredicate (string)
+void nodeQP::addPredicate (int)
 {
 }
 //////////////////////////////////////////////////////////////
@@ -110,9 +130,12 @@ int main ()
 	nodeQP node1 (0,1);
 	nodeQP node2 (1,2);
 	aip aipRegistry;
-	aipRegistry.addPredicate(0,"prod1","hello");
+	aipRegistry.addPredicate(0,"prod1",0);
 	cout << "Node ID: " << node1.nodeId << " TYPE: " << node1.nodeType << endl;
 	cout << "Node ID: " << node2.nodeId << " TYPE: " << node2.nodeType << endl;
+        aipRegistry.checkPredicate(1);
+        aipRegistry.checkPredicate(0);
         cout << "AIP REGISTRY" << endl << endl;
+        aipRegistry.showFullRegistry();
 	exit(0);
 }
