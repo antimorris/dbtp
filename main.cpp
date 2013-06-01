120fb226ed750f23340d53af68b0914aba5e6db8
Javier <antimorris@jusanet.org>
2013-06-01 02:37:26 +0200

    Changed the definition of the parameters for the query plan for Operation : use O-OPERATION  i.e.  "O-JOIN","xxxxx"

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@ int main ()
     //*****************************************
     //Create and load the initial database
     //*****************************************
-    /*
+
+    // I have commented this part temporary to avoid the loading of the database each single test
+        /*
         table BL2013_F;
         table BL2013_F1599;
         table BL2013_F2599;
@@ -28,25 +30,24 @@ int main ()
 
         table test = JOIN(BL2013_F1599,BL2013_MF,"year");
         cout<<test.tuples.size()<<endl;
-
-*/
+        */
 
 	// *************************************
         // Creation of Query Plan for Example 1
         // *************************************
         QueryPlan queryPlan;
-        queryPlan.addNode(0,0,"O","JOIN,partkey");
-        queryPlan.addNode(1,0,"O","DISTINCT,partkey");
-        queryPlan.addNode(2,1,"O","PROJECTION,partkey");
+        queryPlan.addNode(0,0,"O-JOIN","partkey");
+        queryPlan.addNode(1,0,"O-DISTINCT","partkey");
+        queryPlan.addNode(2,1,"O-PROJECTION","partkey");
         queryPlan.addNode(3,2,"P","2*supplycost,<,retailprice");
-        queryPlan.addNode(4,3,"O","JOIN,retailprice");
+        queryPlan.addNode(4,3,"O-JOIN","retailprice");
         queryPlan.addNode(5,4,"T","P");
         queryPlan.addNode(6,4,"T","PS1");
         queryPlan.addNode(7,0,"P","10*avail,<,numsold");
-        queryPlan.addNode(8,7,"O","JOIN,partkey");
-        queryPlan.addNode(9,8,"O","SUM(availqty) as avail partkey");
+        queryPlan.addNode(8,7,"O-JOIN","partkey");
+        queryPlan.addNode(9,8,"Y","SUM(availqty) as avail partkey");
         queryPlan.addNode(10,9,"T","PS2");
-        queryPlan.addNode(11,8,"O","SUM(availqty) as numsold partkey");
+        queryPlan.addNode(11,8,"Y","SUM(availqty) as numsold partkey");
         queryPlan.addNode(12,11,"P","receiptdate,>,2007-1-1,AND,OTHER");
         queryPlan.addNode(13,12,"T","L");
         queryPlan.createPredLists();
